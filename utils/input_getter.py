0f65f7cf193c6d28ad3c4cec6e515ee155b78dcf
lucas <lucasefernandes333@gmail.com>
2024-03-10 17:34:02 -0300

    Add get_matrix function

--- utils/input_getter.py
+++ utils/input_getter.py
@@ -9,3 +9,17 @@ def get_array(length: int) -> np.array:
     for i in range(length):
         array[i] = input_getter()
     return array
+
+
+
+def get_matrix(rows: int, columns: int) -> np.ndarray:
+    matrix = np.array([[0 for i in range(columns)] for j in range(rows)])
+    for i in range(rows):
+        print(f"Enter the {columns} elements of row {i + 1}")
+        for j in range(columns):
+            matrix[i][j] = input_getter()
+
+    return matrix
+
+
+
